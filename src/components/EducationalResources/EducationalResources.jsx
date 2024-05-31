38599ef8a3920cc6af87bd79789d340c5e8c34c3
Chamini Palliyaguru <126881879+chaminipalliyaguru@users.noreply.github.com>
2024-05-31 23:48:03 +0530

    Add files via upload

---
+++ src/components/EducationalResources/EducationalResources.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button } from "@material-tailwind/react";
+import Header from '../Header';
+
+function EducationalResources() {
+  return (
+    <div className="flex w-max gap-4">
+        <Header />
+      <Button color="green">color green</Button>
+    </div>
+  )
+}
+
+export default EducationalResources
